f3a257c192b70526e37cb885030f9fad7bc7688e
Illia Polosukhin <ilblackdragon@gmail.com>
2015-05-03 22:43:32 +0000

    Example code from todo project.

---
+++ computegraph-web/README.md
@@ -0,0 +1,15 @@
+ComputeGraph Web
+===================================
+
+A frontend for ComputeGraph.
+
+Installation
+------------
+
+```sh
+$ npm install
+$ npm run build
+$ npm start
+```
+
+Goto [http://localhost:3000/](http://localhost:3000/).
---
+++ computegraph-web/shared/actions/TodoActions.js
@@ -0,0 +1,91 @@
+import {Actions} from 'flummox';
+import request from 'superagent';
+
+const API_HOST = 'http://videntum.com:3000';
+
+class TodoActions extends Actions {
+    getTasks() {
+        return new Promise((resolve, reject) => {
+            request.get(`${API_HOST}/api/tasks`)
+                .end((error, response) => {
+                    if (error) return reject(error);
+                    resolve(response.body);
+                });
+        });
+    }
+
+    getActiveTasks() {
+        return new Promise((resolve, reject) => {
+            request.get(`${API_HOST}/api/tasks/active`)
+                .end((error, response) => {
+                    if (error) return reject(error);
+                    resolve(response.body);
+                });
+        });
+    }
+
+    getCompletedTasks() {
+        return new Promise((resolve, reject) => {
+            request.get(`${API_HOST}/api/tasks/completed`)
+                .end((error, response) => {
+                    if (error) return reject(error);
+                    resolve(response.body);
+                });
+        });
+    }
+
+    deleteCompletedTasks() {
+        return new Promise((resolve, reject) => {
+            request.del(`${API_HOST}/api/tasks/completed`)
+                .end((error, response) => {
+                    if (error) return reject(error);
+                    resolve(response.body);
+                });
+        });
+    }
+
+    createTask(task) {
+        return new Promise((resolve, reject) => {
+            request.post(`${API_HOST}/api/tasks`)
+                .send(task)
+                .end((error, response) => {
+                    if (error) return reject(error);
+                    resolve(response.body);
+                });
+        });
+    }
+
+    deleteTask(id) {
+        return new Promise((resolve, reject) => {
+            request.del(`${API_HOST}/api/tasks/${id}`)
+                .end((error, response) => {
+                    if (error) return reject(error);
+                    resolve(response.body);
+                });
+        });
+    }
+
+    toggleTask(id, completed) {
+        return new Promise((resolve, reject) => {
+            request.put(`${API_HOST}/api/tasks/${id}`)
+                .send({completed})
+                .end((error, response) => {
+                    if (error) return reject(error);
+                    resolve(response.body);
+                });
+        });
+    }
+
+    toggleAll(completed) {
+        return new Promise((resolve, reject) => {
+            request.put(`${API_HOST}/api/tasks`)
+                .send({completed})
+                .end((error, response) => {
+                    if (error) return reject(error);
+                    resolve(response.body);
+                });
+        });
+    }
+}
+
+export default TodoActions;
